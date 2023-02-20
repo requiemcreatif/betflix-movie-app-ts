c9885097d4b4165c416d2d59fb57b4c29c606f42
requiemcreatif <requiemcreatif@gmail.com>
2023-02-20 13:29:00 +0100

    hero intro done

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -4,18 +4,20 @@ import "../../styles/hero/hero.css";
 
 const Hero = () => {
   return (
-    <div>
-      <div>
-        <img src={betflix} alt="" />
-        <h1>The ultimate movie-watching experience.</h1>
-        <h2>
-          Welcome to our exciting new movie collection application! We are thrilled to provide you
-          with a unique and user-friendly experience that will take your movie-watching to the next
-          level.
-        </h2>
-        <Link to="/home">
-          <button> GET STARTED</button>
-        </Link>
+    <div className="hero-wrapper">
+      <div className="container">
+        <div className="hero-container">
+          <img src={betflix} alt="logo Betflix" />
+          <h1>The ultimate movie-watching experience.</h1>
+          <h3>
+            Welcome to our exciting new movie collection application! We are thrilled to provide you
+            with a unique and user-friendly experience that will take your movie-watching to the
+            next level.
+          </h3>
+          <Link to="/home">
+            <button> GET STARTED</button>
+          </Link>
+        </div>
       </div>
     </div>
   );
